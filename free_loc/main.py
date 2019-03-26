e6631c0c3ccbf3351693e5d78046d0dae7888d3d
Sohil <savla.sohil@gmail.com>
2019-03-25 22:09:56 -0400

    Unsqueeze to add to tensorBoard

--- free_loc/main.py
+++ free_loc/main.py
@@ -333,7 +333,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer, vis, u
                 ind = index.cpu().numpy()[0]
                 heatmapimage_ = output[0,ind]
                 heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                writer.add_image('HeatMap', heatmapimage_, n_iter)
+                writer.add_image('HeatMap', heatmapimage_.unsqueeze(0), n_iter)
                 vis.image( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
             # HeatMap for second one
@@ -341,7 +341,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer, vis, u
                 ind = index.cpu().numpy()[0]
                 heatmapimage_ = output[2,ind]
                 heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                writer.add_image('HeatMap', heatmapimage_, n_iter)
+                writer.add_image('HeatMap', heatmapimage_.unsqueeze(0), n_iter)
                 vis.image( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
         # Same in Visdom with Title: <epoch>_<iteration>_<batch_index>_image, <epoch>_<iteration>_<batch_index>_heatmap_<class_name>
