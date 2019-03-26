0db65d288ce7255c8474c129223e7d3076f26424
Sohil <savla.sohil@gmail.com>
2019-03-25 23:03:01 -0400

    Seed forcing and visdom plotting

--- free_loc/main.py
+++ free_loc/main.py
@@ -88,6 +88,12 @@ parser.add_argument(
     metavar='N',
     help='print frequency (default: 10)')
 parser.add_argument(
+    '--epoch-plot-freq',
+    default=2,
+    type=int,
+    metavar='N',
+    help='epoch plot frequency (default: 2)')
+parser.add_argument(
     '--resume',
     default='',
     type=str,
@@ -185,6 +191,7 @@ def main():
     mean = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float32)
     std = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float32)
 
+    # Adding manual seed to force repeatability
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(5)
 
@@ -325,40 +332,40 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer, vis, u
         #TODO: Visualize things as mentioned in handout
         #TODO: Visualize at appropriate intervals
         n_iter = epoch*len(train_loader) + i
+        if(epoch % args.epoch_plot_freq == 0):
+            # Plot the Training Loss
+            writer.add_scalar('train/loss', loss.mean().item(), n_iter)
+            writer.add_scalar('train/metric1', m1[0], n_iter)
+            writer.add_scalar('train/metric2', m2[0], n_iter)
+            writer.add_scalar('train/metric1avg', avg_m1.avg, n_iter)
+            writer.add_scalar('train/metric2avg', avg_m2.avg, n_iter)
 
-        # Plot the Training Loss
-        writer.add_scalar('train/loss', loss.mean().item(), n_iter)
-        writer.add_scalar('train/metric1', m1[0], n_iter)
-        writer.add_scalar('train/metric2', m2[0], n_iter)
-        writer.add_scalar('train/metric1avg', avg_m1.avg, n_iter)
-        writer.add_scalar('train/metric2avg', avg_m2.avg, n_iter)
+            # Plot images and heat maps of GT classes for 4 batches (2 images in each batch)
+            if( i % 4 == 0 and i>0 and i<20):
+                writer.add_image('Image1', unnormalize(input[0]), n_iter)
+                writer.add_image('Image2', unnormalize(input[2]), n_iter)
 
-        # Plot images and heat maps of GT classes for 4 batches (2 images in each batch)
-        if( i % 4 == 0 and i>0 and i<20):
-            writer.add_image('Image1', unnormalize(input[0]), n_iter)
-            writer.add_image('Image2', unnormalize(input[2]), n_iter)
+                # HeatMap for first one
+                for index in target[0].nonzero():
+                    ind = index.cpu().numpy()[0]
+                    heatmapimage_ = output[0,ind]
+                    heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
+                    writer.add_image('HeatMap1', heatmapimage_.unsqueeze(0), n_iter)
+                    vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
-            # HeatMap for first one
-            for index in target[0].nonzero():
-                ind = index.cpu().numpy()[0]
-                heatmapimage_ = output[0,ind]
-                heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                writer.add_image('HeatMap1', heatmapimage_.unsqueeze(0), n_iter)
-                vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
+                # HeatMap for second one
+                for index in target[2].nonzero():
+                    ind = index.cpu().numpy()[0]
+                    heatmapimage_ = output[2,ind]
+                    heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
+                    writer.add_image('HeatMap2', heatmapimage_.unsqueeze(0), n_iter)
+                    vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
-            # HeatMap for second one
-            for index in target[2].nonzero():
-                ind = index.cpu().numpy()[0]
-                heatmapimage_ = output[2,ind]
-                heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                writer.add_image('HeatMap2', heatmapimage_.unsqueeze(0), n_iter)
-                vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
+            # Same in Visdom with Title: <epoch>_<iteration>_<batch_index>_image, <epoch>_<iteration>_<batch_index>_heatmap_<class_name>
+                vis.image( convert_0_1(input[0]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
+                vis.image( convert_0_1(input[2]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
 
-        # Same in Visdom with Title: <epoch>_<iteration>_<batch_index>_image, <epoch>_<iteration>_<batch_index>_heatmap_<class_name>
-            vis.image( convert_0_1(input[0]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
-            vis.image( convert_0_1(input[2]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
-
-        # End of train()
+            # End of train()
 
 
 def validate(val_loader, model, criterion, epoch, writer, vis, unnormalize):
@@ -415,8 +422,10 @@ def validate(val_loader, model, criterion, epoch, writer, vis, unnormalize):
         #TODO: Visualize things as mentioned in handout
         #TODO: Visualize at appropriate intervals
         n_iter = epoch*len(val_loader) + i
-
-        writer.add_scalar('test/loss', loss.mean().item(), n_iter)
+        if(epoch % args.epoch_plot_freq == 0):
+            writer.add_scalar('test/loss', loss.mean().item(), n_iter)
+            writer.add_scalar('test/meanmetric1', avg_m1.avg, n_iter)
+            writer.add_scalar('test/meanmetric2', avg_m2.avg, n_iter)
 
         # if( i % 4 == 0 and i>0 and i<20):
         #     writer.add_image('Image', unnormalize(input[0]), n_iter)
