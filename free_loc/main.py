12e2914de7cd80e988821417c41a3e3411022d5a
Sohil <savla.sohil@gmail.com>
2019-03-27 03:15:32 -0400

    Finalizing for 1.6

--- free_loc/main.py
+++ free_loc/main.py
@@ -349,42 +349,41 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer, vis, u
         #TODO: Visualize things as mentioned in handout
         #TODO: Visualize at appropriate intervals
         n_iter = epoch*len(train_loader) + i
-        if(epoch % args.epoch_plot_freq == 0):
-            # Plot the Training Loss
-            writer.add_scalar('train/loss', loss.mean().item(), n_iter)
-            writer.add_scalar('train/metric1', m1[0], n_iter)
-            writer.add_scalar('train/metric2', m2[0], n_iter)
-            writer.add_scalar('train/meanmetric1', avg_m1.avg, n_iter)
-            writer.add_scalar('train/meanmetric2', avg_m2.avg, n_iter)
+        # Plot the Training Loss
+        writer.add_scalar('train/loss', loss.mean().item(), n_iter)
+        writer.add_scalar('train/metric1', m1[0], n_iter)
+        writer.add_scalar('train/metric2', m2[0], n_iter)
+        writer.add_scalar('train/meanmetric1', avg_m1.avg, n_iter)
+        writer.add_scalar('train/meanmetric2', avg_m2.avg, n_iter)
 
-            for tag, value in model.named_parameters():
-                tag = tag.replace('.', '/')
-                writer.add_histogram(tag+'/grad', value.grad.data.cpu().numpy(), n_iter)
+        for tag, value in model.named_parameters():
+            tag = tag.replace('.', '/')
+            writer.add_histogram(tag+'/grad', value.grad.data.cpu().numpy(), n_iter)
 
-            # Plot images and heat maps of GT classes for 4 batches (2 images in each batch)
-            if( i % 4 == 0 and i>0 and i<20):
-                writer.add_image('Image1', unnormalize(input[0]), n_iter)
-                writer.add_image('Image2', unnormalize(input[2]), n_iter)
+        # Plot images and heat maps of GT classes for 4 batches (2 images in each batch)
+        if( i % 4 == 0 and i>0 and i<20):
+            writer.add_image('Image1', unnormalize(input[0]), n_iter)
+            writer.add_image('Image2', unnormalize(input[2]), n_iter)
 
-                # HeatMap for first one
-                for index in target[0].nonzero():
-                    ind = index.cpu().numpy()[0]
-                    heatmapimage_ = output[0,ind]
-                    heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                    writer.add_image('HeatMap1', heatmapimage_.unsqueeze(0), n_iter)
-                    vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
+            # HeatMap for first one
+            for index in target[0].nonzero():
+                ind = index.cpu().numpy()[0]
+                heatmapimage_ = output[0,ind]
+                heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
+                writer.add_image('HeatMap1', heatmapimage_.unsqueeze(0), n_iter)
+                vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
-                # HeatMap for second one
-                for index in target[2].nonzero():
-                    ind = index.cpu().numpy()[0]
-                    heatmapimage_ = output[2,ind]
-                    heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
-                    writer.add_image('HeatMap2', heatmapimage_.unsqueeze(0), n_iter)
-                    vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
+            # HeatMap for second one
+            for index in target[2].nonzero():
+                ind = index.cpu().numpy()[0]
+                heatmapimage_ = output[2,ind]
+                heatmapimage_ = display_heatmap(heatmapimage_, input.size()[2:])
+                writer.add_image('HeatMap2', heatmapimage_.unsqueeze(0), n_iter)
+                vis.heatmap( heatmapimage_,opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_heatmap_'+str(class_names[ind])))
 
-                # Same in Visdom with Title: <epoch>_<iteration>_<batch_index>_image, <epoch>_<iteration>_<batch_index>_heatmap_<class_name>
-                vis.image( convert_0_1(input[0]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
-                vis.image( convert_0_1(input[2]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
+            # Same in Visdom with Title: <epoch>_<iteration>_<batch_index>_image, <epoch>_<iteration>_<batch_index>_heatmap_<class_name>
+            vis.image( convert_0_1(input[0]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
+            vis.image( convert_0_1(input[2]),opts=dict(title=str(epoch)+'_'+str(n_iter)+'_'+str(i)+'_image'))
 
             # End of train()
 
