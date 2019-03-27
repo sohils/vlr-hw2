3788ffd97e00681658d50bc6da5b85bdc64a9dd0
Sohil <savla.sohil@gmail.com>
2019-03-27 10:20:48 -0400

    Another update

--- free_loc/custom.py
+++ free_loc/custom.py
@@ -294,8 +294,10 @@ def compute_f1(gt, pred):
     for cid in range(nclasses):
         gt_cls = gt[:, cid].astype('float32')
         pred_cls = pred[:, cid].astype('float32')
-        if(np.count_nonzero(gt_cls) == 0 or np.count_nonzero(pred_cls)==0):
+        if(np.count_nonzero(gt_cls) == 0):
             continue
+        elif(np.count_nonzero(pred_cls)==0):
+            fscore = 0
         else:
             fscore = sklearn.metrics.f1_score(gt_cls,pred_cls)
         F_score.append(fscore)
