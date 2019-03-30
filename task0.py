b876d0cb9f78e4b922137e840e04ca2635fefb66
Sohil <savla.sohil@gmail.com>
2019-03-29 21:04:55 -0400

    Updating task0 script

--- task0.py
+++ task0.py
@@ -18,8 +18,11 @@ rects = imdb._load_pascal_annotation(imdb.image_index[2019])['boxes']
 img = cv2.imread(image_path)
 for r in rects:
     cv2.rectangle(img,(r[0],r[1]),(r[2],r[3]),(0,255,0))
+cv2.imwrite('task0_4',img)
 
+img = cv2.imread(image_path)
 dbs = imdb.selective_search_roidb()
 max_score_indices = np.argsort(dbs[2019]['boxscores'].squeeze())[::-1][:10]
 for b in dbs[2019]['boxes'][:10]:
     cv2.rectangle(img,(b[0],b[1]),(b[2],b[3]),(0,255,0))
+cv2.imwrite('task0_3',img)
